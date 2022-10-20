94964c9af8e9bd35595e992a8a1b243686b7b8c6
Abhinav <abhikeabhi2002@gmail.com>
2022-10-20 23:54:24 +0530

    Updated app.js and data

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const fruits = require("./fruits.json")
-const cors=require("cors")
+const fruits = require("./data/fruits.json")
+const vegetables = require("./data/vegetables.json")
+const frozen = require("./data/frozen.json")
+const cors = require("cors")
 
 const corsOptions ={
    origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
+   credentials:true,            
    optionSuccessStatus:200,
 }
 
@@ -14,6 +16,16 @@ app.use(cors(corsOptions))
 app.get('/fruits', (req, res) => {
     res.json(fruits)
     console.log(fruits)
+})
+
+app.get('/vegetables', (req, res) => {
+    res.json(vegetables)
+    console.log(vegetables)
+})
+
+app.get('/frozen', (req, res) => {
+    res.json(frozen)
+    console.log(frozen)
 })
 
 app.listen(5000, () => {
